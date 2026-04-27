a74c2fe77d0266a2da0f4bd0121310cbeed9e755
Tan123-bot-sudo <tanisha.patra@outlook.com>
2026-04-27 20:26:56 +0530

    Add files via upload

---
+++ Java Assignment 1/Printname.java
@@ -0,0 +1,13 @@
+ class PrintName {
+    String name;
+
+    void display() {
+        System.out.println("My Name is: " + name);
+    }
+
+    public static void main(String[] args) {
+        PrintName obj = new PrintName();
+        obj.name = "Tanisha Patra";
+        obj.display();
+    }
+}
